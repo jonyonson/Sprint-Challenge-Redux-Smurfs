dbf0272bfd986560921e05d33d1ad7630b6fdf15
Jonathan Taylor <jonyonson@icloud.com>
2019-05-17 15:05:45 -0500

    should be working

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,4 +1,4 @@
-/* 
+/*
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
@@ -13,3 +13,45 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+import axios from 'axios';
+
+export const FETCH_SMURFS_START = 'FETCH_SMURFS_START';
+export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
+export const FETCH_SMURFS_FAILURE = 'FETCH_SMURFS_FAILURE';
+
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: FETCH_SMURFS_START });
+
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(res => {
+      dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({
+        type: FETCH_SMURFS_FAILURE,
+        payload: err,
+      });
+    });
+};
+
+export const ADD_SMURF_START = 'ADD_SMURF_START';
+export const ADD_SMURF_SUCCESS = 'ADD_SMURF_SUCCESS';
+export const ADD_SMURF_FAILURE = 'ADD_SMURF_FAILURE';
+
+export const addSmurf = smurf => dispatch => {
+  dispatch({ type: ADD_SMURF_START });
+
+  return axios
+    .post('http://localhost:3333/smurfs', smurf)
+    .then(res => {
+      dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({
+        type: FETCH_SMURFS_FAILURE,
+        payload: err,
+      });
+    });
+};
--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import {
-  FaRegTrashAlt as TrashIcon,
-  FaRegEdit as EditIcon,
-} from 'react-icons/fa';
 
 const Smurf = props => {
   return (
@@ -10,16 +6,6 @@ const Smurf = props => {
       <h3 className="Smurf__name">{props.smurf.name}</h3>
       <p className="Smurf__height">{props.smurf.height} tall</p>
       <p className="Smurf__age">{props.smurf.age} smurf years old</p>
-      <EditIcon
-        // onClick={() => props.handleUpdate(props.id)}
-        className="Smurf__icon-edit"
-        size="20"
-      />
-      <TrashIcon
-        // onClick={() => props.deleteSmurf(props.id)}
-        className="Smurf__icon-trash"
-        size="20"
-      />
     </div>
   );
 };
